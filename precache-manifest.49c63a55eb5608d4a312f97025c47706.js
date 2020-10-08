746d24f806b91a66be5f316fcbe7e5dfb808a530
garethscott <59648675+garethscott@users.noreply.github.com>
2020-10-08 19:22:32 +0100

    Updates

---
+++ precache-manifest.49c63a55eb5608d4a312f97025c47706.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7ef989b760fcc6402fc3a98f210a7e",
+    "url": "/Porfolio/index.html"
+  },
+  {
+    "revision": "9367fba6e697ce53bfb2",
+    "url": "/Porfolio/static/css/main.e01ef76b.chunk.css"
+  },
+  {
+    "revision": "e4750b6f77c79807013c",
+    "url": "/Porfolio/static/js/2.4353f4bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Porfolio/static/js/2.4353f4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9367fba6e697ce53bfb2",
+    "url": "/Porfolio/static/js/main.0a4becf8.chunk.js"
+  },
+  {
+    "revision": "560612c5a33c7af80131",
+    "url": "/Porfolio/static/js/runtime-main.5130a4e5.js"
+  },
+  {
+    "revision": "2da085e6bfb75bcfc0ec63cd80191c6e",
+    "url": "/Porfolio/static/media/Picture-Project-AddBook.2da085e6.jpg"
+  },
+  {
+    "revision": "750a332e22ae500d8a272597bbf67f2a",
+    "url": "/Porfolio/static/media/Picture-Project-Calc.750a332e.jpg"
+  },
+  {
+    "revision": "46c952b2c80d568faedb68fe8cc93f66",
+    "url": "/Porfolio/static/media/Picture-Project-DiceGame.46c952b2.jpg"
+  },
+  {
+    "revision": "f4cad728af70fc6314357b63a3d639b6",
+    "url": "/Porfolio/static/media/Picture-Project-Drumkit.f4cad728.jpg"
+  },
+  {
+    "revision": "61a1986675c47f506dcc23694a26c64d",
+    "url": "/Porfolio/static/media/Picture-Project-Keycode.61a19866.png"
+  },
+  {
+    "revision": "d3f0811fa02abb6b9124121ec4e9142f",
+    "url": "/Porfolio/static/media/Picture-Project-Poke.d3f0811f.jpg"
+  },
+  {
+    "revision": "36fd1b0e02a83a4857317ac1f8ce272b",
+    "url": "/Porfolio/static/media/Picture-Project-TodoApp.36fd1b0e.jpg"
+  },
+  {
+    "revision": "cc871c436bd793be63f6dc3f35f3151c",
+    "url": "/Porfolio/static/media/Picture-Project-TravelApp.cc871c43.jpg"
+  },
+  {
+    "revision": "313ff3a56e983525725e16e4aff01b36",
+    "url": "/Porfolio/static/media/Project-Square-AddBook.313ff3a5.jpg"
+  },
+  {
+    "revision": "e2a74b30d3b222248ae6f044a6bb8e4b",
+    "url": "/Porfolio/static/media/Project-Square-Calc.e2a74b30.jpg"
+  },
+  {
+    "revision": "5cdab2c9ce177592090f9160da0c1856",
+    "url": "/Porfolio/static/media/Project-Square-DiceGame.5cdab2c9.jpg"
+  },
+  {
+    "revision": "2c0b5f043ace572fb5c947ad5329f8f9",
+    "url": "/Porfolio/static/media/Project-Square-DrumKit.2c0b5f04.jpg"
+  },
+  {
+    "revision": "e22bdea5cab0c1ed9a71bca99c27fb81",
+    "url": "/Porfolio/static/media/Project-Square-Keycode-2.e22bdea5.jpg"
+  },
+  {
+    "revision": "88b3ce3a4ad34ac760eabcfb9a8221c2",
+    "url": "/Porfolio/static/media/Project-Square-Poke.88b3ce3a.jpg"
+  },
+  {
+    "revision": "a4282478d706b6ec9ccef028509b3bc3",
+    "url": "/Porfolio/static/media/Project-Square-Todo.a4282478.jpg"
+  },
+  {
+    "revision": "9f6e32ede30a686d8c8e28fb1a96a39f",
+    "url": "/Porfolio/static/media/Project-Square-WeatherApp.9f6e32ed.jpg"
+  },
+  {
+    "revision": "9c4a329a163da8d6d1b172b518513319",
+    "url": "/Porfolio/static/media/Project-Squares-TravelApp.9c4a329a.jpg"
+  },
+  {
+    "revision": "5790eedbe97ff36e04fadd29837b7b2d",
+    "url": "/Porfolio/static/media/about-img.5790eedb.jpg"
+  },
+  {
+    "revision": "5f1a1dc90dbb1b13e384c6494ef245cf",
+    "url": "/Porfolio/static/media/landing-image-1.5f1a1dc9.jpg"
+  },
+  {
+    "revision": "39b09a595ea40982910875224faa6528",
+    "url": "/Porfolio/static/media/projects-image-1.39b09a59.png"
+  },
+  {
+    "revision": "347601ba00fed1faeb30357be197876a",
+    "url": "/Porfolio/static/media/projects-type-blob.347601ba.png"
+  }
+]);
